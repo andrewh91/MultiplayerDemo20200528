de179607efcd3007105f3c815c662a63bfdecc9e
andrewh91 <andrewahughes@googlemail.com>
2020-05-28 12:42:16 +0100

    Got this working, although not mentioned in the tutorial, to run 2 desktop launchers at once; need to run the first with coverage , which is a button 2 to the right of the normal run icon, then run the second one normally. Alternatively go to run, edit configurations, at the left panel select desktop, now at the top right tick allow parallel run, this lets you do ‘run with coverage’ or more. Either way seems to work at the moment This ‘coverage’ appears to be something to do with testing ... Now each player that connects to the server is given a new id, each player already in the server is made aware of the new player’s id. In the android studio Run panel, there will be a tab for each instance of the desktop app running, each will have messages displaying that player’s id. tut part 4 complete https://www.youtube.com/watch?v=0t2wlvBJBUw

--- core/src/com/gmail/andrewahughes/MyGdxGame.java
+++ core/src/com/gmail/andrewahughes/MyGdxGame.java
@@ -5,25 +5,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	private Socket socket;
+	String id;
 
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		System.out.println("create");
-		System.out.println("connect...");
 		connectSocket();
+		configSocketEvents();
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
@@ -31,15 +36,51 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.end();
 	}
 
-	public void connectSocket(){
-		try{
+	public void connectSocket() {
+		try {
 			socket = IO.socket("http://localhost:8080");
 			socket.connect();
 			System.out.println("connected");
-		} catch(Exception e){
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
+
+	public void configSocketEvents() {
+		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+			/*when something connects to the server*/
+			@Override
+			public void call(Object... args) {
+				Gdx.app.log("SocketIO", "Connected");
+			}
+		}).on("socketID", new Emitter.Listener() {
+			/*the event listener will listen for an event called "socketID"*/
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					id = data.getString("id");
+					Gdx.app.log("SocketIO", "my ID: " + id);
+				} catch(JSONException e){
+					Gdx.app.log("SocketIO","Error getting ID" +e);
+				}
+			}
+		}).on("newPlayer", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					id = data.getString("id");
+					Gdx.app.log("SocketIO", "New Player connected: " + id);
+				} catch(JSONException e){
+					Gdx.app.log("SocketIO","Error getting new Player ID" +e);
+				}
+			}
+		});
+
+	}
+
+
 
 
 	@Override
--- index.js
+++ index.js
@@ -9,6 +9,14 @@ server.listen(8080, function(){
 
 io.on('connection', function(socket){
 	console.log("Player Connected!");
+	/*when player connects server will automatically emit
+	a unique socket id to that player, i've named it "socketID"*/
+	socket.emit('socketID', { id: socket.id });
+	/*the player will be connected on it's socket,
+	we have just sent it an id using the socket
+	we can send to all other players on different sockets by
+	using broadcast as below*/
+	socket.broadcast.emit('newPlayer', { id: socket.id });
 	socket.on('disconnect', function(){
 		console.log("Player Disconnected");
 	});
