7321ccf996d654dec426aca5c1441348612676b8
andrewh91 <andrewahughes@googlemail.com>
2020-05-28 13:47:48 +0100

    This one went pretty easy without a hitch, now we can move the badlogic sprite left and right, then when we load a second game, the first game will will display the second player, second player only sees himself however, similar to how the second player is not informed of the first player’s id. this was part 5 https://www.youtube.com/watch?v=fC7mgZQGbiU&t=6s

--- core/src/com/gmail/andrewahughes/MyGdxGame.java
+++ core/src/com/gmail/andrewahughes/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.gmail.andrewahughes;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,33 +10,66 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 	private Socket socket;
 	String id;
+	Starship player;
+	Texture playerShip;
+	Texture friendlyShip;
+	/*the hashmap will store the a key which in our case is a string
+	 and a value which for us is a Starship class
+	* string will store the id , starship will store a starship class*/
+	HashMap<String, Starship> friendlyPlayers;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		playerShip = new Texture("badlogic.jpg");
+		friendlyShip = new Texture("badlogic.jpg");
+		friendlyPlayers = new HashMap<String, Starship>();
 		connectSocket();
 		configSocketEvents();
+	}
+	public void handleInput(float dt){
+		if(player != null) {
+			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+				player.setPosition(player.getX() + (-200 * dt), player.getY());
+			} else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+				player.setPosition(player.getX() + (+200 * dt), player.getY());
+			}
+		}
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		handleInput(Gdx.graphics.getDeltaTime());
 		batch.begin();
-		batch.draw(img, 0, 0);
+		if(player != null){
+			player.draw(batch);
+		}
+		/*for each entry in the hash map, get the value, which remember
+		* is a starship class, and call the function draw to draw that
+		* starship.  note that starship inherits draw method from texture*/
+		for(HashMap.Entry<String, Starship> entry : friendlyPlayers.entrySet()){
+			entry.getValue().draw(batch);
+		}
 		batch.end();
 	}
-
+	@Override
+	public void dispose () {
+		batch.dispose();
+		playerShip.dispose();
+		friendlyShip.dispose();
+	}
 	public void connectSocket() {
 		try {
 			socket = IO.socket("http://localhost:8080");
@@ -52,6 +86,7 @@ public class MyGdxGame extends ApplicationAdapter {
 			@Override
 			public void call(Object... args) {
 				Gdx.app.log("SocketIO", "Connected");
+				player = new Starship(playerShip);
 			}
 		}).on("socketID", new Emitter.Listener() {
 			/*the event listener will listen for an event called "socketID"*/
@@ -71,9 +106,11 @@ public class MyGdxGame extends ApplicationAdapter {
 				JSONObject data = (JSONObject) args[0];
 				try {
 					id = data.getString("id");
-					Gdx.app.log("SocketIO", "New Player connected: " + id);
-				} catch(JSONException e){
-					Gdx.app.log("SocketIO","Error getting new Player ID" +e);
+					Gdx.app.log("SocketIO", "New Player Connect: " + id);
+					/*put the new player's id and the starship class in our hashmap*/
+					friendlyPlayers.put(id, new Starship(friendlyShip));
+				}catch(JSONException e){
+					Gdx.app.log("SocketIO", "Error getting New PlayerID");
 				}
 			}
 		});
@@ -83,9 +120,5 @@ public class MyGdxGame extends ApplicationAdapter {
 
 
 
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
